85bd4afb58443024e24604f8e5b2c63f45fd19dc
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2020-12-28 22:14:54 +0300

    committtttttее

--- src/main/java/ab/kg/Bot.java
+++ src/main/java/ab/kg/Bot.java
@@ -2,8 +2,13 @@ package ab.kg;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Bot extends TelegramLongPollingBot {
@@ -18,7 +23,8 @@ public class Bot extends TelegramLongPollingBot {
                     .setChatId(chat_id)
                     .setText("salam АЙДААНА "+new Random(47).nextInt());
             try {
-                execute(message); // Sending our message object to user
+                addingBtn(message,message.deserializeResponse("aloha salam"));
+                execute(message); // отправка СМС
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -32,5 +38,41 @@ public class Bot extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
         return "1227975503:AAH8Yw4qJXp5Ylm1shw3Z9btQfYTIYfKTxc";
+    }
+
+
+    public void addingBtn(SendMessage message,Message msg){
+        message.enableMarkdown(true);
+
+        // Создаем клавиуатуру
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        message.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        // Создаем список строк клавиатуры
+        List<KeyboardRow> keyboard = new ArrayList<>();
+
+        // Первая строчка клавиатуры
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        // Добавляем кнопки в первую строчку клавиатуры
+        keyboardFirstRow.add("Кнопка Алтынбек");
+        keyboardFirstRow.add("Кнопка Айдаана");
+
+        // Вторая строчка клавиатуры
+        KeyboardRow keyboardSecondRow = new KeyboardRow();
+        // Добавляем кнопки во вторую строчку клавиатуры
+        keyboardSecondRow.add("Айыл банк");
+        keyboardSecondRow.add("Мастер кард");
+
+        // Добавляем все строчки клавиатуры в список
+        keyboard.add(keyboardFirstRow);
+        keyboard.add(keyboardSecondRow);
+        // и устанваливаем этот список нашей клавиатуре
+        replyKeyboardMarkup.setKeyboard(keyboard);
+
+
+        message.setReplyToMessageId(msg.getMessageId());
     }
 }
