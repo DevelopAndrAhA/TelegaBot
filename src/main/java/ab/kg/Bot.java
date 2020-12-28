cb441293258d9eee22ffad12dbfc39ee188f30cb
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2020-12-28 21:50:27 +0300

    committttttt

--- src/main/java/ab/kg/Bot.java
+++ src/main/java/ab/kg/Bot.java
@@ -1,17 +1,12 @@
 package ab.kg;
 
-import ab.kg.service.MyService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.util.List;
+import java.util.Random;
 
 public class Bot extends TelegramLongPollingBot {
-    @Autowired
-    MyService myService;
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
@@ -19,14 +14,10 @@ public class Bot extends TelegramLongPollingBot {
             String message_text = update.getMessage().getText();
             System.out.println(message_text);
             long chat_id = update.getMessage().getChatId();
-            List l = myService.getAllEntities();
-            String msgTxt="";
-            for(int i=0;i<l.size();i++){
-                msgTxt = msgTxt+l.get(i).toString();
-            }
+
             SendMessage message = new SendMessage() // Create a message object object
                     .setChatId(chat_id)
-                    .setText(msgTxt);
+                    .setText("TEXT FROM ALTUHA ,RANDOM INT "+new Random(47).nextInt());
             try {
                 execute(message); // Sending our message object to user
             } catch (Exception e) {
